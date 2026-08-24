65ca6ac8104af5182f1b6a9a92e465f94bb22c60
unikmo <mbanwie@googlemail.com>
2026-08-24 19:13:55 +0200

    Add accessible mobile navigation component

---
+++ app/MobileMenu.tsx
@@ -0,0 +1,17 @@
+export default function MobileMenu() {
+  return (
+    <details className="mobileNav">
+      <summary>Menu</summary>
+      <div className="mobileNavPanel">
+        <a href="/the-chaos-walk">The Walk</a>
+        <a href="/cities">Cities</a>
+        <a href="/join">Join first-access</a>
+        <a href="/shop">Chaos Fan</a>
+        <a href="/city-leads">City Leads</a>
+        <a href="/guides">Guides</a>
+        <a href="/the-cause">The Cause</a>
+        <a href="/privacy">Privacy</a>
+      </div>
+    </details>
+  );
+}
